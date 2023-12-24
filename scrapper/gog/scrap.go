bbb2d0bfce23eb76b85c17467fee6a06eae40ed0
Alvin Yeoh <alvinyah1@spgroup.com.sg>
2023-12-24 12:32:21 +0800

    Add pagination scrapping for gog

--- scrapper/gog/scrap.go
+++ scrapper/gog/scrap.go
@@ -1,6 +1,7 @@
 package gog
 
 import (
+	"log"
 	"net/url"
 	"strconv"
 	"strings"
@@ -28,12 +29,34 @@ func NewScrapper() scrapper.Scrapper {
 }
 
 func (s Store) Scrap(searchStr string) ([]scrapper.Card, error) {
+	var (
+		err   error
+		cards []scrapper.Card
+	)
+
+	pagesMap := make(map[int]string)
 	searchURL := s.BaseUrl + s.SearchUrl + url.QueryEscape(searchStr)
-	var cards []scrapper.Card
 
 	c := colly.NewCollector()
 
-	c.OnHTML("div.collectionGrid", func(e *colly.HTMLElement) {
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		// get pagination
+		e.ForEach("ol.pagination li", func(_ int, el *colly.HTMLElement) {
+			elStr := strings.Replace(el.Text, "«", "", -1)
+			elStr = strings.Replace(elStr, "page", "", -1)
+			elStr = strings.Replace(elStr, "Next", "", -1)
+			elStr = strings.Replace(elStr, "Previous", "", -1)
+			elStr = strings.Replace(elStr, "»", "", -1)
+			elStr = strings.TrimSpace(elStr)
+			if elStr != "" && elStr != "1" && el.ChildAttr("a", "href") != "" {
+				elInt, strConvErr := strconv.Atoi(elStr)
+				if strConvErr == nil {
+					pagesMap[elInt] = el.ChildAttr("a", "href")
+				}
+			}
+		})
+
+		// get cards
 		e.ForEach("div.productCard__card", func(_ int, el *colly.HTMLElement) {
 			var (
 				isInstock bool
@@ -72,5 +95,63 @@ func (s Store) Scrap(searchStr string) ([]scrapper.Card, error) {
 		})
 	})
 
-	return cards, c.Visit(searchURL)
+	err = c.Visit(searchURL)
+	if err != nil {
+		return []scrapper.Card{}, err
+	}
+
+	if len(pagesMap) > 0 {
+		log.Println("Pagination exists for "+s.Name+": ", pagesMap)
+
+		for _, url := range pagesMap {
+			searchURL = s.BaseUrl + url
+			c2 := colly.NewCollector()
+
+			c2.OnHTML("div.collectionGrid", func(e *colly.HTMLElement) {
+				e.ForEach("div.productCard__card", func(_ int, el *colly.HTMLElement) {
+					var (
+						isInstock bool
+						price     float64
+					)
+
+					// in stock
+					if len(el.ChildTexts("div.productCard__button--outOfStock")) == 0 {
+						isInstock = true
+					}
+
+					if isInstock {
+						el.ForEach("ul.productChip__grid li", func(_ int, el2 *colly.HTMLElement) {
+							if el2.Attr("data-variantavailable") == "true" && el2.Attr("data-variantqty") != "0" {
+								priceStr := el2.Attr("data-variantprice")
+								priceStr = strings.Replace(priceStr, "$", "", -1)
+								priceStr = strings.Replace(priceStr, ",", "", -1)
+								priceStr = strings.Replace(priceStr, "SGD", "", -1)
+								price, _ = strconv.ParseFloat(strings.TrimSpace(priceStr), 64)
+								price = price / 100
+
+								if price > 0 {
+									cards = append(cards, scrapper.Card{
+										Name:    strings.TrimSpace(el.ChildText("p.productCard__title")),
+										Url:     strings.TrimSpace(s.BaseUrl + el.ChildAttr("a", "href")),
+										InStock: isInstock,
+										Price:   price,
+										Source:  s.Name,
+										Img:     strings.TrimSpace("https:" + el.ChildAttr("img", "data-src")),
+										Quality: el2.Attr("data-varianttitle"),
+									})
+								}
+							}
+						})
+					}
+				})
+			})
+
+			err = c2.Visit(searchURL)
+			if err != nil {
+				break
+			}
+		}
+	}
+
+	return cards, err
 }
