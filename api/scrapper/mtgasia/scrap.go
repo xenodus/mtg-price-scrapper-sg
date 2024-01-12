7c798123782a8b6e1af4a14519a6ddb486381cfe
Alvin Yeoh <alvinyah1@spgroup.com.sg>
2024-01-12 17:38:35 +0800

    Bugfix mtg asia multiple quality for 1 card

--- api/scrapper/mtgasia/scrap.go
+++ api/scrapper/mtgasia/scrap.go
@@ -40,41 +40,42 @@ func (s Store) Scrap(searchStr string) ([]scrapper.Card, error) {
 
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		e.ForEach("table#cardTable div.accordion", func(_ int, el *colly.HTMLElement) {
-			var (
-				isInstock bool
-				price     float64
-				quality   string
-			)
+			// only proceed if name found
+			if len(el.ChildTexts("span.missingText")) > 0 {
+				el.ForEach("table.cardHeader", func(_ int, el2 *colly.HTMLElement) {
+					var (
+						isInstock bool
+						price     float64
+						quality   string
+					)
+					if len(el2.ChildTexts("td")) == 2 {
+						priceStr := el2.ChildTexts("td")[0]
+						priceStr = strings.Replace(priceStr, "SG$", "", -1)
+						priceStr = strings.Replace(priceStr, ",", "", -1)
+						price, _ = strconv.ParseFloat(strings.TrimSpace(priceStr), 64)
 
-			el.ForEach("table.cardHeader", func(_ int, el2 *colly.HTMLElement) {
-				if len(el2.ChildTexts("td")) == 2 {
-
-					priceStr := el2.ChildTexts("td")[0]
-					priceStr = strings.Replace(priceStr, "SG$", "", -1)
-					priceStr = strings.Replace(priceStr, ",", "", -1)
-					price, _ = strconv.ParseFloat(strings.TrimSpace(priceStr), 64)
-
-					if price > 0 {
-						isInstock = true
-						quality = el2.ChildTexts("td")[1]
+						if price > 0 {
+							isInstock = true
+							quality = el2.ChildTexts("td")[1]
+						}
 					}
-				}
-			})
 
-			if isInstock {
-				if el.ChildText("span.foilText") != "" {
-					quality += " " + el.ChildText("span.foilText")
-				}
+					if isInstock {
+						if el.ChildText("span.foilText") != "" {
+							quality += " " + el.ChildText("span.foilText")
+						}
 
-				cards = append(cards, scrapper.Card{
-					Name:    strings.TrimSpace(el.ChildText("span.missingText")),
-					Url:     searchURL,
-					InStock: isInstock,
-					Price:   price,
-					Source:  s.Name,
-					// MTG Asia is not on https so can't use their image
-					// Img:     strings.TrimSpace(StoreBaseURL + el.ChildAttr("img", "src")),
-					Quality: strings.TrimSpace(quality),
+						cards = append(cards, scrapper.Card{
+							Name:    strings.TrimSpace(el.ChildTexts("span.missingText")[0]),
+							Url:     searchURL,
+							InStock: isInstock,
+							Price:   price,
+							Source:  s.Name,
+							// MTG Asia is not on https so can't use their image
+							// Img:     strings.TrimSpace(StoreBaseURL + el.ChildAttr("img", "src")),
+							Quality: strings.TrimSpace(quality),
+						})
+					}
 				})
 			}
 		})
