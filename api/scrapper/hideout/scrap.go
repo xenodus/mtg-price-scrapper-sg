61f5b65ab53775b1c6970d3b279bbd63d6aa937f
Alvin Yeoh <alvinyah1@spgroup.com.sg>
2025-03-21 18:35:30 +0800

    Switch from scrap to api for hideout

--- api/scrapper/hideout/scrap.go
+++ api/scrapper/hideout/scrap.go
@@ -1,17 +1,17 @@
 package hideout
 
 import (
-	"net/url"
-	"strconv"
-	"strings"
+	"encoding/json"
 
-	"github.com/gocolly/colly/v2"
 	"mtg-price-scrapper-sg/scrapper"
+	"mtg-price-scrapper-sg/scrapper/binderpos"
 )
 
 const StoreName = "Hideout"
 const StoreBaseURL = "https://hideoutcg.com"
 const StoreSearchURL = "/search?q="
+
+const binderposStoreURL = "220022-20.myshopify.com"
 
 type Store struct {
 	Name      string
@@ -28,49 +28,63 @@ func NewScrapper() scrapper.Scrapper {
 }
 
 func (s Store) Scrap(searchStr string) ([]scrapper.Card, error) {
-	searchURL := s.BaseUrl + s.SearchUrl + url.QueryEscape(searchStr)
-	var cards []scrapper.Card
-
-	c := colly.NewCollector()
-
-	c.OnHTML("div.collectionGrid", func(e *colly.HTMLElement) {
-		e.ForEach("div.productCard__card", func(_ int, el *colly.HTMLElement) {
-			var (
-				isInstock bool
-				price     float64
-			)
-
-			// not out of stock
-			if el.ChildText("form") != "" {
-				isInstock = true
-
-				if isInstock {
-					el.ForEach("ul.productChip__grid li", func(_ int, el2 *colly.HTMLElement) {
-						if el2.Attr("data-variantavailable") == "true" && el2.Attr("data-variantqty") != "0" {
-							priceStr := el2.Attr("data-variantprice")
-							priceStr = strings.Replace(priceStr, "$", "", -1)
-							priceStr = strings.Replace(priceStr, ",", "", -1)
-							priceStr = strings.Replace(priceStr, "SGD", "", -1)
-							price, _ = strconv.ParseFloat(strings.TrimSpace(priceStr), 64)
-							price = price / 100
-
-							if price > 0 {
-								cards = append(cards, scrapper.Card{
-									Name:    strings.TrimSpace(el.ChildText("p.productCard__title")),
-									Url:     strings.TrimSpace(s.BaseUrl + el.ChildAttr("a", "href")),
-									InStock: isInstock,
-									Price:   price,
-									Source:  s.Name,
-									Img:     strings.TrimSpace("https:" + el.ChildAttr("img", "data-src")),
-									Quality: el2.Attr("data-varianttitle"),
-								})
-							}
-						}
-					})
-				}
-			}
-		})
+	reqPayload, err := json.Marshal(binderpos.Payload{
+		StoreURL:    binderposStoreURL,
+		Game:        binderpos.ProductTypeMTG.ToString(),
+		Title:       searchStr,
+		InstockOnly: true,
 	})
+	if err != nil {
+		return []scrapper.Card{}, err
+	}
 
-	return cards, c.Visit(searchURL)
+	return binderpos.GetCards(s.Name, s.BaseUrl, reqPayload)
 }
+
+//func (s Store) Scrap(searchStr string) ([]scrapper.Card, error) {
+//	searchURL := s.BaseUrl + s.SearchUrl + url.QueryEscape(searchStr)
+//	var cards []scrapper.Card
+//
+//	c := colly.NewCollector()
+//
+//	c.OnHTML("div.collectionGrid", func(e *colly.HTMLElement) {
+//		e.ForEach("div.productCard__card", func(_ int, el *colly.HTMLElement) {
+//			var (
+//				isInstock bool
+//				price     float64
+//			)
+//
+//			// not out of stock
+//			if el.ChildText("form") != "" {
+//				isInstock = true
+//
+//				if isInstock {
+//					el.ForEach("ul.productChip__grid li", func(_ int, el2 *colly.HTMLElement) {
+//						if el2.Attr("data-variantavailable") == "true" && el2.Attr("data-variantqty") != "0" {
+//							priceStr := el2.Attr("data-variantprice")
+//							priceStr = strings.Replace(priceStr, "$", "", -1)
+//							priceStr = strings.Replace(priceStr, ",", "", -1)
+//							priceStr = strings.Replace(priceStr, "SGD", "", -1)
+//							price, _ = strconv.ParseFloat(strings.TrimSpace(priceStr), 64)
+//							price = price / 100
+//
+//							if price > 0 {
+//								cards = append(cards, scrapper.Card{
+//									Name:    strings.TrimSpace(el.ChildText("p.productCard__title")),
+//									Url:     strings.TrimSpace(s.BaseUrl + el.ChildAttr("a", "href")),
+//									InStock: isInstock,
+//									Price:   price,
+//									Source:  s.Name,
+//									Img:     strings.TrimSpace("https:" + el.ChildAttr("img", "data-src")),
+//									Quality: el2.Attr("data-varianttitle"),
+//								})
+//							}
+//						}
+//					})
+//				}
+//			}
+//		})
+//	})
+//
+//	return cards, c.Visit(searchURL)
+//}
